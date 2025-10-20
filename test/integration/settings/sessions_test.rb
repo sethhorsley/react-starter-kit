91a825f46fc5437f2aa2476194f3fdaf8840c5f9
Seth Horsley <github.dvxhi@sl.isethi.com>
2025-10-20 15:11:16 -0400

    remove rspec add mini test
    s

---
+++ test/integration/settings/sessions_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Settings::SessionsTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in_as @user
+  end
+
+  test "GET /index returns success" do
+    get settings_sessions_url
+    assert_response :success
+  end
+end
